61f7319eeafd61dbf4d4acf0b7dad735c23c885e
GirishChauhan15 <girishchauhan333@mail.com>
2025-06-23 16:58:26 +0530

    Update TailwindCSS

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ function App() {
 
   return (
     <>
-      <h1>Lorem ipsum dolor sit amet.</h1>
+      <h1 className="text-5xl text-emerald-500 text-center mt-10">Lorem ipsum dolor sit amet.</h1>
     </>
   )
 }
